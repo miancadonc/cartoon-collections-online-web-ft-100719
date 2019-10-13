b1796948d1f57a4de788d228801a677edcc8119e
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 02:00:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,10 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+  array.any? {|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
